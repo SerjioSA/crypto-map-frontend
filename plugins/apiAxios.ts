f3df50cc7c59d615386973b405c8f48f8938bac3
SerjioSA <37222543+SerjioSA@users.noreply.github.com>
2023-08-29 13:41:45 +0400

    - deleted base usr

--- plugins/apiAxios.ts
+++ plugins/apiAxios.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 export default defineNuxtPlugin((nuxtApp) => {
-  const defaultUrl = "http://127.0.0.1:8000/";
 
   let api = axios.create({
-    baseURL: defaultUrl,
     headers: {
       common: {},
     },
